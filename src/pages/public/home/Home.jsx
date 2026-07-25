65577d735861e3e05b41e15c99c5df4be5755779
vansh12125 <vanshsahu6752@gmail.com>
2026-07-25 16:00:20 +0530

    Home Page change

--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -1,13 +1,14 @@
 import { Scene3D } from "@/components/three";
 import UIOverlay from "./UIOverlay";
-
 export default function Home() {
   return (
-    <>
-      <div className="fixed inset-0 z-0 pointer-events-none">
+    <div className="relative w-full min-h-screen bg-[#050507] overflow-x-hidden">
+      {}
+      <div className="fixed inset-0 z-0 pointer-events-auto">
         <Scene3D />
       </div>
+      {}
       <UIOverlay />
-    </>
+    </div>
   );
 }
